450f4fe86dce3f6b4705598dd06bb3e491532523
浅墨兰 <344330607@edufree.win>
2025-08-17 09:08:35 +0800

    修改配置文件。

--- Basic/src/main/java/com/letter/basic/fragment/BaseMultiStateVBVMReceiverFragment.kt
+++ Basic/src/main/java/com/letter/basic/fragment/BaseMultiStateVBVMReceiverFragment.kt
@@ -1,4 +1,4 @@
-package com.energy.sources.base.fragment
+package com.letter.basic.fragment
 
 import android.content.Context
 import android.content.Intent
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
-import com.letter.basic.fragment.BaseMultiStateVBVMFragment
 import com.letter.basic.manager.ReceiverManager
 import com.letter.basic.receiver.ReceiverImpl
 
