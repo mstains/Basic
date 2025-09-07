ef0a63372682441dc8161dfa8a2968c3e62c41e8
浅墨兰 <344330607@edufree.win>
2025-09-07 12:43:42 +0800

    优化相关代码

--- app/src/main/java/com/letter/basic/MainActivity.kt
+++ app/src/main/java/com/letter/basic/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.letter.basic
 
-import android.os.Bundle
+
 import android.view.LayoutInflater
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.letter.basic.activity.BaseMultiStateVBActivity
+import androidx.fragment.app.FragmentActivity
+
 import com.letter.basic.databinding.ActivityMainBinding
 
-class MainActivity : BaseMultiStateVBActivity<ActivityMainBinding>() {
-    /**
-     * 创建viewBinding
-     * */
-    override fun createViewBinding(inflater: LayoutInflater): ActivityMainBinding {
-        return ActivityMainBinding.inflate(inflater)
-    }
+class MainActivity :FragmentActivity(){
 
-    /**
-     * 初始化状态栏
-     * */
-    override fun initStatusBar() {
 
-    }
 
 
 }
+//class MainActivity : BaseMultiStateVBActivity<ActivityMainBinding>() {
+//    /**
+//     * 创建viewBinding
+//     * */
+//    override fun createViewBinding(inflater: LayoutInflater): ActivityMainBinding {
+//        return ActivityMainBinding.inflate(inflater)
+//    }
+//
+//    /**
+//     * 初始化状态栏
+//     * */
+//    override fun initStatusBar() {
+//
+//    }
+//
+//
+//}
